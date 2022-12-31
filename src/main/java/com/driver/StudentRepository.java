cc8aa4e401db34e853cbdbd41fae62f63c7a8a2e
Shubham Singh <shubhamsin.464@gmail.com>
2022-12-31 11:57:30 +0530

    try

---
+++ src/main/java/com/driver/StudentRepository.java
@@ -0,0 +1,84 @@
+package com.driver;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@Repository
+public class StudentRepository {
+    HashMap<String,Student> studentDb = new HashMap<>();
+    HashMap<String,Teacher> teacherDb = new HashMap<>();
+    HashMap<String,List<String>> pairsDb = new HashMap<>();
+
+    public void addStudentToDb(Student student){
+    studentDb.put(student.getName(),student);
+    }
+    public void addTeacherToDb(Teacher teacher){
+        teacherDb.put(teacher.getName(),teacher);
+    }
+    public void addStudentTeacherPair(String teacher,String student){
+        if(studentDb.containsKey(student)&&teacherDb.containsKey(teacher)){
+            if(pairsDb.containsKey(teacher)){
+                List<String> ls = pairsDb.get(teacher);
+                ls.add(student);
+                pairsDb.put(teacher,ls);
+            }else{
+                List<String> ls = new ArrayList<>();
+                ls.add(student);
+                pairsDb.put(teacher,ls);
+            }
+        }
+    }
+    public Student getStudentByName(String name){
+         return studentDb.get(name);
+    }
+    public Teacher getTeacherByName(String name){
+        return teacherDb.get(name);
+    }
+    public List<String> getStudentsByTeacherName(String name){
+        return pairsDb.get(name);
+    }
+    public List<String> getAllStudents(){
+        List<String> temp= new ArrayList<>();
+        temp.addAll(studentDb.keySet());
+        return temp;
+    }
+    public void deleteTeacherByName(String teacher){
+        if(teacherDb.containsKey(teacher)){
+            if(pairsDb.containsKey(teacher)){
+                List<String> sudent=pairsDb.get(teacher);
+                for(String s:sudent)
+                {
+                    if(studentDb.containsKey(s))
+                    {
+                        studentDb.remove(s);
+                    }
+                }
+                teacherDb.remove(teacher);
+                pairsDb.remove(teacher);
+            }
+        }
+    }
+    public void deleteAllTeachers() {
+        for(String teacher:teacherDb.keySet()){
+            if(pairsDb.containsKey(teacher))
+            {
+                List<String> StudentList=pairsDb.get(teacher);
+                for(String ls:StudentList)
+                {
+                    if(studentDb.containsKey(ls))
+                    {
+                        studentDb.remove(ls);
+                    }
+                }
+            }
+            pairsDb.clear();
+            teacherDb.clear();;
+        }
+        pairsDb.clear();
+        teacherDb.clear();
+    }
+}
+
---
+++ src/main/java/com/driver/StudentService.java
@@ -0,0 +1,21 @@
+package com.driver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+    @Autowired
+    StudentRepository studentRepository;
+    public void addStudent(Student student){studentRepository.addStudentToDb(student);}
+    public void addTeacher(Teacher teacher){studentRepository.addTeacherToDb(teacher);}
+    public void addStudentTeacherPair(String teacher,String student){studentRepository.addStudentTeacherPair(teacher,student);}
+    public Student getStudentByName(String name){ return studentRepository.getStudentByName(name);}
+    public Teacher getTeacherByName(String name){return studentRepository.getTeacherByName(name);}
+    public List<String> getStudentsByTeacherName(String teacher){return studentRepository.getStudentsByTeacherName(teacher);}
+    public List<String> getAllStudents(){return  studentRepository.getAllStudents();}
+    public void deleteTeacherByName(String teacher) {studentRepository.deleteTeacherByName(teacher);}
+    public void deleteAllTeachers() {studentRepository.deleteAllTeachers();}
+}
